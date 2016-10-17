7a7ce6bf1b0b18d962e16ba55898dc12ce34ad16
Ethan Huang <cherub0526@gmail.com>
2016-10-18 03:40:52 +0800

    update

--- ruby-on-rails/install.sh
+++ ruby-on-rails/install.sh
@@ -8,24 +8,39 @@ apt-get install -y build-essential git-core bison openssl libreadline6-dev curl 
 zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3  autoconf libc6-dev \
 libpcre3-dev curl libcurl4-nss-dev libxml2-dev libxslt-dev imagemagick nodejs libffi-dev
 
-# 用套件快速安裝 Ruby
+# 安裝 Ruby
 apt-get install -y --force-yes software-properties-common
 apt-add-repository ppa:brightbox/ruby-ng -y
 apt-get update
-apt-get install -y --force-yes ruby2.3 ruby2.3-dev
+apt-get install -y ruby2.3 ruby2.3-dev
+
+# install mysql
+#apt-get install mysql-common mysql-client libmysqlclient-dev
+
+
+# install postgreSql
+#apt-get install postgresql libpq-dev postgresql-contrib
 
 # 安裝 Nginx + Passenger
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7 -y
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
 
 apt-get install -y --force-yes apt-transport-https ca-certificates
 
-# Add our APT repository
+# Add APT repository
 sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main > /etc/apt/sources.list.d/passenger.list'
 
 apt-get update
 
 # Install Passenger + Nginx
-apt-get install -y --force-yes nginx-extras passenger
+apt-get install -y nginx-extras passenger
 
 service nginx restart
+
+# install latest git
+apt-add-repository ppa:git-core/ppa -y
+apt-get update
+apt-get install -y git
+
+# install bundler
+gem install bundler
 
